b94b16aa9649423f6f2d165fb1111a8d9791cbce
Hienb1812267 <hienb1812267@student.ctu.edu.vn>
2021-12-19 09:26:50 +0700

    update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask_cors import CORS, cross_origin
 import load_model as MODEL
-# import process as PREPROCESS
+import process as PREPROCESS
 # import pandas as pd
 # import firebase_admin
 # from firebase_admin import credentials, firestore
