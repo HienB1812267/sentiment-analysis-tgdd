8109d935c4d8a683b509a52831e990edb51e0ffa
Hienb1812267 <hienb1812267@student.ctu.edu.vn>
2021-12-19 09:40:01 +0700

    update

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask_cors import CORS, cross_origin
 import load_model as MODEL
-# import process as PREPROCESS
+import process as PREPROCESS
 import pandas as pd
-# import firebase_admin
-# from firebase_admin import credentials, firestore
+import firebase_admin
+from firebase_admin import credentials, firestore
 
 # Use a service account
-# cred = credentials.Certificate('.\sentiment-prediction-tgdd-firebase-adminsdk-olmlq-105780f3e4.json')
-# firebase_admin.initialize_app(cred)
+cred = credentials.Certificate('.\sentiment-prediction-tgdd-firebase-adminsdk-olmlq-105780f3e4.json')
+firebase_admin.initialize_app(cred)
 
-# db = firestore.client()
+db = firestore.client()
 
 #Init
 app = Flask(__name__)
@@ -24,59 +24,59 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route('/')
 @cross_origin(origin='*')
 def index_process():
-    return "123"
+    return render_template("index.html")
 
-# @app.route('/predict', methods=["POST", "GET"])
-# @cross_origin(origin='*')
-# def predict_star():
-#     comment = request.form['input_comment']
-#     username = request.form['input_name']
-#     if len(username) == 0:
-#         username = "Anonymous"
-#     #preprocess data
-#     df = pd.DataFrame()
-#     df['comment'] = [comment]
-#     df['comment'] = df['comment'].apply(PREPROCESS.preProcessing)
-#     cmt = df['comment']
-#     #load vectorizes
-#     count_vector, tf_idf_vector = MODEL.load_vectorizes()
-#     input_cv = count_vector.transform(cmt)
-#     input_tf = tf_idf_vector.transform(cmt)
+@app.route('/predict', methods=["POST", "GET"])
+@cross_origin(origin='*')
+def predict_star():
+    comment = request.form['input_comment']
+    username = request.form['input_name']
+    if len(username) == 0:
+        username = "Anonymous"
+    #preprocess data
+    df = pd.DataFrame()
+    df['comment'] = [comment]
+    df['comment'] = df['comment'].apply(PREPROCESS.preProcessing)
+    cmt = df['comment']
+    #load vectorizes
+    count_vector, tf_idf_vector = MODEL.load_vectorizes()
+    input_cv = count_vector.transform(cmt)
+    input_tf = tf_idf_vector.transform(cmt)
 
-#     output = []
-#     #Ta sẽ dùng mô hình logistic với tf-idf để dự đoán chính. Các mô hình còn lại sẽ được lưu vào database
-#     # để phục vụ cho chức năng xem chi tiết nếu người dùng muốn xem chi tiết dự đoán của các mô hình#
+    output = []
+    #Ta sẽ dùng mô hình logistic với tf-idf để dự đoán chính. Các mô hình còn lại sẽ được lưu vào database
+    # để phục vụ cho chức năng xem chi tiết nếu người dùng muốn xem chi tiết dự đoán của các mô hình#
 
-#     #load bayes
-#     bayes_cv, bayes_tf = MODEL.load_bayes_model()
-#     #Lưu lại dự đoán
-#     output.append(bayes_cv.predict(input_cv)[0])
-#     output.append(bayes_tf.predict(input_tf)[0])
+    #load bayes
+    bayes_cv, bayes_tf = MODEL.load_bayes_model()
+    #Lưu lại dự đoán
+    output.append(bayes_cv.predict(input_cv)[0])
+    output.append(bayes_tf.predict(input_tf)[0])
 
-#     #load svm
-#     svm_cv, svm_tf = MODEL.load_svm_model()
-#     #Lưu lại dự đoán
-#     output.append(svm_cv.predict(input_cv)[0])
-#     output.append(svm_tf.predict(input_tf)[0])
+    #load svm
+    svm_cv, svm_tf = MODEL.load_svm_model()
+    #Lưu lại dự đoán
+    output.append(svm_cv.predict(input_cv)[0])
+    output.append(svm_tf.predict(input_tf)[0])
 
-#     #load svm
-#     logistic_cv, logistic_tf = MODEL.load_svm_model()
-#     #Lưu lại dự đoán
-#     output.append(logistic_cv.predict(input_cv)[0])
-#     output.append(logistic_tf.predict(input_tf)[0])
-#     #connect db
-#     doc_ref = db.collection(u'data').document()
-#     doc_ref.set({
-#         u'comment': comment,
-#         u'username': username,
-#         u'bayes_cv': int(output[0]),
-#         u'bayes_tf': int(output[1]),
-#         u'svm_cv': int(output[2]),
-#         u'svm_tf': int(output[3]),
-#         u'logis_cv': int(output[4]),
-#         u'logis_tf': int(output[5]),
-#     })
-#     return redirect("/")
+    #load svm
+    logistic_cv, logistic_tf = MODEL.load_svm_model()
+    #Lưu lại dự đoán
+    output.append(logistic_cv.predict(input_cv)[0])
+    output.append(logistic_tf.predict(input_tf)[0])
+    #connect db
+    doc_ref = db.collection(u'data').document()
+    doc_ref.set({
+        u'comment': comment,
+        u'username': username,
+        u'bayes_cv': int(output[0]),
+        u'bayes_tf': int(output[1]),
+        u'svm_cv': int(output[2]),
+        u'svm_tf': int(output[3]),
+        u'logis_cv': int(output[4]),
+        u'logis_tf': int(output[5]),
+    })
+    return redirect("/")
 
 
 #Start backend
